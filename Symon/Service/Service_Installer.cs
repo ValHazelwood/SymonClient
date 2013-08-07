e6bdc89cdeb8bc24e8725738aa2661110f7142ad
ValHazelwood <master.leva@gmail.com>
2013-08-07 12:27:33 +0400

    Added CopyRight string

--- Symon/Service/Service_Installer.cs
+++ Symon/Service/Service_Installer.cs
@@ -1,16 +1,42 @@
+/*
+ * Copyright (c) 20011-2013 Val Hazelwood
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 using System.ComponentModel;
 using System.Configuration.Install;
 using System.ServiceProcess;
 
 namespace Symon
 {
-    /// <summary>
-    /// Mail server service installer.
-    /// </summary>
     [RunInstaller(true)]
     public class Symon_Service_Installer : Installer
     {
-        /// Required designer variable.
         private System.ComponentModel.Container components = null;
 
         private ServiceInstaller serviceInstaller;
@@ -21,10 +47,8 @@ namespace Symon
         /// </summary>
         public Symon_Service_Installer()
         {
-            // This call is required by the Designer.
             InitializeComponent();
-
-            // TODO: Add any initialization after the InitComponent call
+            
             processInstaller = new ServiceProcessInstaller();
             serviceInstaller = new ServiceInstaller();
 
@@ -41,17 +65,12 @@ namespace Symon
             Installers.Add(serviceInstaller);
             Installers.Add(processInstaller);
         }
-
-        #region Component Designer generated code
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+        
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
         }
-        #endregion
 
     }
 }
+
